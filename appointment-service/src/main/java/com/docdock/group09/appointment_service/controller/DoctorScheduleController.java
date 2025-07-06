2f29e86f08bd9257d8ff60406aa9994610c78f73
huytng1603 <anhdevwen@gmail.com>
2025-07-06 23:06:05 +0700

    Refactor appointment service

--- appointment-service/src/main/java/com/docdock/group09/appointment_service/controller/DoctorScheduleController.java
+++ appointment-service/src/main/java/com/docdock/group09/appointment_service/controller/DoctorScheduleController.java
@@ -1,5 +1,6 @@
 package com.docdock.group09.appointment_service.controller;
 
+import com.docdock.group09.appointment_service.dto.response.DocDockResponse;
 import com.docdock.group09.appointment_service.service.DoctorScheduleService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
@@ -19,18 +20,18 @@ public class DoctorScheduleController {
         if (date == null) {
             date = LocalDate.now();
         }
-        return ResponseEntity.ok(doctorScheduleService.getDoctorScheduleIn7days(doctorId, date));
+        return DocDockResponse.returnSuccess(doctorScheduleService.getDoctorScheduleIn7days(doctorId, date));
     }
 
     @GetMapping("/seed")
     public ResponseEntity<?> mockData() {
         doctorScheduleService.bulkInsertData();
-        return ResponseEntity.ok().build();
+        return DocDockResponse.returnSuccess("Mock data successfully");
     }
 
     @GetMapping("/available")
     public ResponseEntity<?> getAvailableTimes(@RequestParam(name = "doctorId") String doctorId,
                                                @RequestParam(name = "date") LocalDate date) {
-        return ResponseEntity.ok(doctorScheduleService.getTodayAvailable(doctorId, date));
+        return DocDockResponse.returnSuccess(doctorScheduleService.getTodayAvailable(doctorId, date));
     }
 }
