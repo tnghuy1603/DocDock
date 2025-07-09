0ee522025f9d7cdb28349a562ee324e6f1da1ddf
huytng1603 <anhdevwen@gmail.com>
2025-07-10 00:30:43 +0700

    Add medicalRecordId to prescription

--- medical-record-service/src/main/java/com/docdock/group09/medical_record_service/exception/GlobalExceptionHandler.java
+++ medical-record-service/src/main/java/com/docdock/group09/medical_record_service/exception/GlobalExceptionHandler.java
@@ -1,12 +1,14 @@
 package com.docdock.group09.medical_record_service.exception;
 
 import com.docdock.group09.medical_record_service.dto.response.DocDockResponse;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
 @ControllerAdvice
+@Slf4j
 public class GlobalExceptionHandler {
     @ExceptionHandler(MedicalRecordServiceException.class)
     public ResponseEntity<?> handleException(MedicalRecordServiceException ex) {
@@ -15,6 +17,7 @@ public class GlobalExceptionHandler {
 
     @ExceptionHandler(Exception.class)
     public ResponseEntity<?> handleException(Exception ex) {
+        log.info(ex.getMessage(), ex);
         return DocDockResponse.returnError("Internal server error", HttpStatus.INTERNAL_SERVER_ERROR.value());
     }
 }
