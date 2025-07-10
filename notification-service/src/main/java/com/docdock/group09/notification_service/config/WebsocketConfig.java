46a87f3021aabe208f9fce9ef4fecd9383ac02c4
huytng1603 <anhdevwen@gmail.com>
2025-07-11 01:32:31 +0700

    allow cors for websocket

--- notification-service/src/main/java/com/docdock/group09/notification_service/config/WebsocketConfig.java
+++ notification-service/src/main/java/com/docdock/group09/notification_service/config/WebsocketConfig.java
@@ -12,7 +12,7 @@ public class WebsocketConfig implements WebSocketMessageBrokerConfigurer {
     @Override
     public void registerStompEndpoints(StompEndpointRegistry registry) {
         registry.addEndpoint("/ws")
-                .setAllowedOrigins("*")
+                .setAllowedOrigins("http://localhost:5173")
                 .withSockJS();
     }
 
