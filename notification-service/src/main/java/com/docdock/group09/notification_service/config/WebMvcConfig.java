3f6086f74d8d2f058bbea6f99129645e232c03ad
huytng1603 <anhdevwen@gmail.com>
2025-07-11 01:10:57 +0700

    allow cors for websocket

---
+++ notification-service/src/main/java/com/docdock/group09/notification_service/config/WebMvcConfig.java
@@ -0,0 +1,21 @@
+package com.docdock.group09.notification_service.config;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+@RequiredArgsConstructor
+public class WebMvcConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("http://localhost:5173")
+                .allowCredentials(true)
+                .allowedMethods("*")
+                .allowedHeaders("*")
+                .allowCredentials(true);
+    }
+}
